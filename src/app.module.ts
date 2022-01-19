13ef2c8da0bff9bcbdd01e1d7d094c163891a860
SelimWebDev <selim.elmahi@gmail.com>
2022-01-19 15:24:12 +0100

    feat(initialization): ajout du script de test e2e pour lancer le bon fichier dans le dossier test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ArrondissementModule } from './arrondissement/arrondissement.module';
 
 @Module({
   imports: [
-    ArrondissementModule,
     MongooseModule.forRoot(
       'mongodb+srv://masterp6:macaron2022@cluster0.ck76z.mongodb.net/mapData?retryWrites=true&w=majority',
     ),
