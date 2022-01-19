375a50a1290731a5e9d52d50f9ac8c94996e88db
SelimWebDev <selim.elmahi@gmail.com>
2022-01-19 13:30:11 +0100

    feat(tournage): implémentation de tournage.module et importation dans app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArrondissementModule } from './arrondissement/arrondissement.module';
+import { TournageModule } from './tournage/tournage.module';
 
 @Module({
   imports: [
     ArrondissementModule,
+    TournageModule,
     MongooseModule.forRoot(
       'mongodb+srv://masterp6:macaron2022@cluster0.ck76z.mongodb.net/mapData?retryWrites=true&w=majority',
     ),
