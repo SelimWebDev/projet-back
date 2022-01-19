2c3cd148dd4599ce60f0e6e433f27dc7013d2f36
SelimWebDev <selim.elmahi@gmail.com>
2022-01-19 11:29:50 +0100

    feat(initialization): package mongoose added

---
+++ src/arrondissement/arrondissement.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ArrondissementController } from './arrondissement.controller';
+import { ArrondissementService } from './arrondissement.service';
+import { ArrondissementSchema } from './arrondissement.model';
+import { ArrondissementRepository } from './arrondissement.repository';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Arrondissement', schema: ArrondissementSchema }]),
+  ],
+  controllers: [ArrondissementController],
+  providers: [ArrondissementService, ArrondissementRepository],
+})
+export class ArrondissementModule {}
