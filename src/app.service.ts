648aa08027ffefd72dcdbdab3e5c38e118526d3f
SelimWebDev <selim.elmahi@gmail.com>
2022-01-20 16:22:50 +0100

    feat(initialization): implémentation mongoose.forRootAsync et logger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger('app.service');
+
   getHello(): string {
+    this.logger.log('return hello world');
     return 'Hello World!';
   }
 }
