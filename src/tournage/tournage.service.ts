667fb07cf7afdbd5a6fcdfd1fd90bddd72223db3
SelimWebDev <selim.elmahi@gmail.com>
2022-01-20 11:48:47 +0100

    feat(tournage): implémentation des NotFoundException

--- src/tournage/tournage.service.ts
+++ src/tournage/tournage.service.ts
@@ -7,23 +7,12 @@ export class TournageService {
 
   // service pour avoir tous les tournages
   async getAll() {
-    let tournages;
-    try {
-      tournages = await this.tournageRepository.findAll();
-    } catch (error) {
-      //log nest
-      throw new Error('Erreur');
-    }
+    const tournages = await this.tournageRepository.findAll();
     return tournages;
   }
 
   async getTournagesByCode(code: string) {
-    let tournages;
-    try {
-      tournages = await this.tournageRepository.findByCode(code);
-    } catch (error) {
-      throw new Error('Erreur');
-    }
+    const tournages = await this.tournageRepository.findByCode(code);
     return tournages;
   }
 }
