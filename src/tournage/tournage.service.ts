c7e51d8c119377b6b45d77564d096f4fa9d20d74
SelimWebDev <selim.elmahi@gmail.com>
2022-01-21 11:43:56 +0100

    feat(arrondissement): implémentation du logger dans app.service et arrondissement.service

--- src/tournage/tournage.service.ts
+++ src/tournage/tournage.service.ts
@@ -3,20 +3,19 @@ import { TournageRepository } from './tournage.repository';
 
 @Injectable()
 export class TournageService {
-
   private readonly logger = new Logger(TournageService.name);
 
   constructor(private readonly tournageRepository: TournageRepository) {}
 
   // service pour avoir tous les tournages
   async getAll() {
-    this.logger.log("get all")
+    this.logger.log('get all');
     const tournages = await this.tournageRepository.findAll();
     return tournages;
   }
 
   async getTournagesByCode(code: string) {
-    this.logger.log("get tournages by code")
+    this.logger.log('get tournages by code');
     const tournages = await this.tournageRepository.findByCode(code);
     return tournages;
   }
