616263f11511a2b285490c5de1b2e1fc13570cd4
SelimWebDev <selim.elmahi@gmail.com>
2022-01-26 14:17:22 +0100

    feat(tournage): implémentation du test e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,7 +5,8 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
+  const nbOfAllTournages = 8919;
+  const nbOfTournagesFiltered = 248;
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,11 +16,26 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('GET tournages', function () {
+    it('responds with array of all tournage', async function () {
+      //test
+      const response = await request(app.getHttpServer()).get('/tournages');
+      //assert
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.status).toEqual(200);
+      expect(response.body.length).toEqual(nbOfAllTournages);
+    });
+
+    it('responds with array of tournage of 75015 arrondissement', async function () {
+      //test
+      const response = await request(app.getHttpServer()).get(
+        '/tournages/75015',
+      );
+      //assert
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.status).toEqual(200);
+      expect(response.body.length).toEqual(nbOfTournagesFiltered);
+    });
   });
 
   afterAll(async () => {
