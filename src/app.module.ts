ebd36c24278a4e722717d5d7a5df1a9a895634af
SelimWebDev <selim.elmahi@gmail.com>
2022-01-26 14:27:03 +0100

    fix(tournage): modification de app.module suite à suppression de fichier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ArrondissementModule } from './arrondissement/arrondissement.module';
 import { TournageModule } from './tournage/tournage.module';
 
 @Module({
   imports: [
-    ArrondissementModule,
     TournageModule,
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
@@ -19,7 +15,7 @@ import { TournageModule } from './tournage/tournage.module';
       inject: [ConfigService],
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
